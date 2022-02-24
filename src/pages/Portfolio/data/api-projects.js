1d670bc1e640d1d454e8293a40c8deeb373b6122
danger2123 <sellmysolenyc@gmail.com>
2022-02-24 00:34:47 -0500

    portfolio: updated/added projects for all files in data folder

--- src/pages/Portfolio/data/api-projects.js
+++ src/pages/Portfolio/data/api-projects.js
@@ -2,17 +2,17 @@ const myApiProjects = [
   {
     id: 1,
     title: "Movie Database",
-    desc: "Using Movie Database API, search your favorite movies to learn more about it.",
-    link: "https://webdevchris-movies.netlify.app/",
     img: "https://res.cloudinary.com/webdevchris/image/upload/v1645069664/portfolio%20site/movie_vutbfq.png",
+    desc: "Using Movie Database API, search your favorite movies to learn more about it",
+    link: "https://webdevchris-movies.netlify.app/",
     code: "https://www.google.com",
   },
   {
     id: 2,
     title: "Cocktail Database",
-    desc: "Using Cocktail Database API, search your favorite drinks and learn how to make them.",
-    link: "https://webdevchris-cocktail-db.netlify.app",
     img: "https://res.cloudinary.com/webdevchris/image/upload/v1645069662/portfolio%20site/cocktail_y0lsiq.png",
+    desc: "Using Cocktail Database API, search your favorite drinks and learn how to make them",
+    link: "https://webdevchris-cocktail-db.netlify.app",
     code: "https://www.google.com",
   },
 ];
